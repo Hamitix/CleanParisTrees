c2b19175e25c415252f84c3439e4b1b1d7825b86
Dylan Hamiti <d.hamiti@groupeonepoint.com>
2022-04-08 14:30:41 +0200

    Initial Commit

---
+++ TreeOP/TreeOPApp.swift
@@ -0,0 +1,17 @@
+//
+//  TreeOPApp.swift
+//  TreeOP
+//
+//  Created by Dylan HAMITI on 08/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct TreeOPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
